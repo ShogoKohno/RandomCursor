7e4aa74ac017e4ca90f90e5ff4ac713e1ab7e20e
shogokohno4616 <shogokohno4616@gmail.com>
2026-07-07 18:24:04 +0900

    refactor: replace registry startup with task scheduler

--- RandomCursor/Services/StartupManager.cs
+++ RandomCursor/Services/StartupManager.cs
@@ -1,76 +1,87 @@
-﻿using Microsoft.Win32;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace RandomCursor.Services;
 
 public static class StartupManager
 {
-    private const string RunKey =
-        @"Software\Microsoft\Windows\CurrentVersion\Run";
-
-    private const string AppName =
-        "RandomCursor";
+    private const string TaskName = "RandomCursor";
 
 
-    /// <summary>
-    /// スタートアップ登録
-    /// </summary>
     public static void Enable(string exePath)
     {
-        using RegistryKey? key =
-            Registry.CurrentUser.OpenSubKey(
-                RunKey,
-                writable: true);
+        string arguments =
+            "--startup-run";
+
+        string taskCommand =
+            $@"/Create /TN ""RandomCursor"" " +
+            $@"/TR ""\""{exePath}\"" {arguments}"" " +
+            "/SC ONLOGON " +
+            "/F";
 
 
-        if (key == null)
-            throw new Exception(
-                "Startup registry unavailable");
+        var process = Process.Start(
+            new ProcessStartInfo
+            {
+                FileName = "schtasks",
+                Arguments = taskCommand,
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            });
 
 
-        key.SetValue(
-            AppName,
-            $"\"{exePath}\"");
+        string output =
+            process!.StandardOutput.ReadToEnd();
+
+        string error =
+            process.StandardError.ReadToEnd();
+
+
+        File.WriteAllText(
+            "startup_debug.txt",
+            $"OUTPUT:\n{output}\nERROR:\n{error}");
     }
 
 
-
-    /// <summary>
-    /// スタートアップ解除
-    /// </summary>
     public static void Disable()
     {
-        using RegistryKey? key =
-            Registry.CurrentUser.OpenSubKey(
-                RunKey,
-                writable: true);
-
-
-        if (key == null)
-            return;
-
-
-        key.DeleteValue(
-            AppName,
-            false);
+        Process.Start(
+            new ProcessStartInfo
+            {
+                FileName = "schtasks",
+                Arguments =
+                    $@"/Delete /TN ""{TaskName}"" /F",
+                CreateNoWindow = true,
+                UseShellExecute = false
+            });
     }
 
 
-
-    /// <summary>
-    /// 登録状態確認
-    /// </summary>
     public static bool IsEnabled()
     {
-        using RegistryKey? key =
-            Registry.CurrentUser.OpenSubKey(
-                RunKey);
+        Process process =
+            Process.Start(
+                new ProcessStartInfo
+                {
+                    FileName = "schtasks",
+                    Arguments =
+                        $@"/Query /TN ""{TaskName}""",
+                    CreateNoWindow = true,
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true
+                })!;
 
 
-        if (key == null)
-            return false;
+        string output =
+            process.StandardOutput.ReadToEnd();
 
 
-        return key.GetValue(
-            AppName) != null;
+        process.WaitForExit();
+
+
+        return output.Contains(
+            TaskName);
     }
 }
